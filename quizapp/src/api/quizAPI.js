052668db928b0b71aceb17c4e44c6c21d6a47a35
Krishna Patel <138808086+missKrishnaPatel@users.noreply.github.com>
2025-08-01 22:19:10 +0530

    Update quizAPI.js

--- quizapp/src/api/quizAPI.js
+++ quizapp/src/api/quizAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://quickquizeapp.onrender.com/api";
 
 export const fetchQuizzes = () => axios.get(`${API_BASE}/quizzes`);
 export const fetchQuizById = (id) => axios.get(`${API_BASE}/quiz/${id}`);
